7df88c4d988039f54d6377e41768ac06d9837909
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-12 16:09:30 +0100

    add exercise 3 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,9 +16,11 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.length;
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
